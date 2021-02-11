e2afe8cba16906fdff4d694d0772ead64f974ff9
Eduardo Macetas <eduardo.macetas@gmail.com>
2021-02-10 22:36:31 -0500

    Adding "reverseArrayInPlace" function :sleepy:

--- Chapter 04/reversingAnArray.js
+++ Chapter 04/reversingAnArray.js
@@ -1,10 +1,22 @@
 function reverseArray(array) {
   let reverse = [];
-  console.log(array.length);
   for (let i = array.length - 1; i >= 0; i -= 1) {
     reverse.push(array[i]);
   }
   return reverse;
 }
 
+function reverseArrayInPlace(array) {
+  for (let i = 0; i < Math.floor(array.length / 2); i++) {
+    let copy = array[i];
+    array[i] = array[array.length - 1 - i];
+    array[array.length - 1 - i] = copy;
+  }
+  return array;
+}
+
 console.log(reverseArray(["A", "B", "C"]));
+
+let arrayValue = [1, 2, 3, 4, 5];
+reverseArrayInPlace(arrayValue);
+console.log(arrayValue);
