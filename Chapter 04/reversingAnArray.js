afaf9cc9e83103bab2f74610cb8210bc64eb4715
Eduardo Macetas <eduardo.macetas@gmail.com>
2021-02-16 20:39:26 -0500

    Adding examples

--- Chapter 04/reversingAnArray.js
+++ Chapter 04/reversingAnArray.js
@@ -1,3 +1,10 @@
+// console.log(reverseArray(["A", "B", "C"]));
+// // → ["C", "B", "A"];
+// let arrayValue = [1, 2, 3, 4, 5];
+// reverseArrayInPlace(arrayValue);
+// console.log(arrayValue);
+// // → [5, 4, 3, 2, 1]
+
 function reverseArray(array) {
   let reverse = [];
   for (let i = array.length - 1; i >= 0; i -= 1) {
