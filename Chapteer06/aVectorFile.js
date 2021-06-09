f59761fbf085d3ae50d4d0f1d2de9822b0ad51cc
Eduardo Macetas <eduardo.macetas@gmail.com>
2021-06-08 23:49:33 -0500

    A Vector File

--- Chapteer06/aVectorFile.js
+++
@@ -1,7 +0,0 @@
-function speak(line) {
-  console.log(`The ${this.type} rabbit says '${line}'`);
-}
-let whiteRabbit = { type: "white", speak };
-let hungryRabbit = { type: "hungry", speak };
-
-whiteRabbit.speak("Oh my ears and whiskers, " + "how late it's getting!");
